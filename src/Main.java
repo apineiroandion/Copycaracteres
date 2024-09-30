f0b75313e0977426e9a91b8cf6a0939e4d6e2932
apineiroandion <apineiroandion@danielcastelao.org>
2024-09-30 09:06:43 +0200

    ejercicio terminado

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
-import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    /**
+     * Ruta del primer archivo
+     */
+    private static String ruta = "/home/dam/Documentos/Copycaracteres/texto1.txt";
+    /**
+     * Ruta del segundo archivo
+     */
+    private static String ruta2 = "/home/dam/Documentos/Copycaracteres/texto2.txt";
 
+    /**
+     * Metodo main
+     * @param args
+     */
+    public static void main(String[] args) {
+        copycaracteres();
+    }
+    /**
+     * Metodo que copia los caracteres de un archivo a otro
+     */
+    public static void copycaracteres() {
+        {
+            try (
+                    FileReader fr = new FileReader(ruta);
+                    FileWriter fw = new FileWriter(ruta2);
+            ) {
+                int caracter = fr.read();
+                while (caracter != -1) {
+                    fw.write(caracter);
+                    caracter = fr.read();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
